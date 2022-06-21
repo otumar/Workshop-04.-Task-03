3c45cb699c3bac13e0e87c4a660c331b4f058cf4
«Тумар <otumar@yandex.ru>
2022-06-22 00:52:00 +0300

    Task 03

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции.
+
+void PrintArray(int [] r)
+{
+    for (int i = 0; i < r.Length; i++)
+    {
+        System.Console.Write(r [i] + " ");
+    }
+}
+int [] array;
+array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(0,1000);
+}
+PrintArray(array);
